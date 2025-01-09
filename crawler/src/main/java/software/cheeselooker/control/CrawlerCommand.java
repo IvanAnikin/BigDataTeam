354fd077e246b0fd1e0247e2206e18ec2693e8a5
IvanAnikin <ivan.anikin@outlook.com>
2025-01-09 17:14:35 +0000

    stage 2

--- crawler/src/main/java/software/cheeselooker/control/CrawlerCommand.java
+++ crawler/src/main/java/software/cheeselooker/control/CrawlerCommand.java
@@ -31,11 +31,11 @@ public class CrawlerCommand implements Command {
         int successfulDownloads = 0;
         downloadLastBooks(successfulDownloads, lastId);
 
-        System.out.println("Five books downloaded successfully.");
+        System.out.println("Three books downloaded successfully.");
     }
 
     private void downloadLastBooks(int successfulDownloads, int lastId) {
-        while (successfulDownloads < 5) {
+        while (successfulDownloads < 3) {
             int nextId = lastId + 1;
             lastId += 1;
 
