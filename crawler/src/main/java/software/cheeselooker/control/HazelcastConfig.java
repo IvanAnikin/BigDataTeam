aa6f01b505df3a57f503fad28546380a21488c2b
IvanAnikin <ivan.anikin@outlook.com>
2025-01-15 16:27:04 +0000

    config

--- crawler/src/main/java/software/cheeselooker/control/HazelcastConfig.java
+++ crawler/src/main/java/software/cheeselooker/control/HazelcastConfig.java
@@ -1,14 +1,10 @@
-
 package software.cheeselooker.control;
-
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.NetworkConfig;
-import com.hazelcast.config.JoinConfig;
+
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.map.IMap;
-
 
 public class HazelcastConfig {
 
@@ -20,17 +16,16 @@ public class HazelcastConfig {
 
         NetworkConfig network = config.getNetworkConfig();
         network.setPort(5701).setPortAutoIncrement(true);
-        network.getJoin().getTcpIpConfig()
-            .addMember("193.145.130.8") 
-            .addMember("193.145.130.7") 
-            .setEnabled(true);
-            
+        
+        // Configure TCP/IP discovery across local network
         network.getJoin().getMulticastConfig().setEnabled(false);
+        network.getJoin().getTcpIpConfig()
+            .addMember("192.168.1.*") // Broad range for local network machines
+            .setEnabled(true);
 
         network.getJoin().getTcpIpConfig().setConnectionTimeoutSeconds(30);
         config.setProperty("hazelcast.max.no.heartbeat.seconds", "60");
         config.setProperty("hazelcast.logging.level", "DEBUG");
-
 
         return Hazelcast.newHazelcastInstance(config);
     }
