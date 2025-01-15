1e93ad56e69719855ca34647f0a9799546707dfe
IvanAnikin <ivan.anikin@outlook.com>
2025-01-15 16:09:12 +0000

    config

--- crawler/src/main/java/software/cheeselooker/control/HazelcastConfig.java
+++ crawler/src/main/java/software/cheeselooker/control/HazelcastConfig.java
@@ -1,14 +1,10 @@
-
 package software.cheeselooker.control;
-
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.config.JoinConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.map.IMap;
-
 
 public class HazelcastConfig {
 
@@ -18,18 +14,26 @@ public class HazelcastConfig {
         config.setClusterName("shared-cluster");
         config.getJetConfig().setEnabled(true);
 
+        // Network configuration
         NetworkConfig network = config.getNetworkConfig();
         network.setPort(5701).setPortAutoIncrement(true);
-        network.getJoin().getTcpIpConfig()
-            .addMember("172.19.0.2") 
-            .setEnabled(true);
-            
-        network.getJoin().getMulticastConfig().setEnabled(false);
 
-        network.getJoin().getTcpIpConfig().setConnectionTimeoutSeconds(30);
+        // Join configuration: Enable multicast
+        JoinConfig join = network.getJoin();
+        join.getMulticastConfig()
+            .setEnabled(true)
+            .setMulticastGroup("224.2.2.3") // Multicast group (can be customized)
+            .setMulticastPort(54327)        // Multicast port
+            .setMulticastTimeoutSeconds(2) // Timeout for multicast discovery
+            .addTrustedInterface("192.168.1.*"); // Optional: restrict to local network
+
+        // Disable other join methods to prevent conflicts
+        join.getTcpIpConfig().setEnabled(false);
+        join.getAwsConfig().setEnabled(false);
+        
+        // Set Hazelcast properties
         config.setProperty("hazelcast.max.no.heartbeat.seconds", "60");
         config.setProperty("hazelcast.logging.level", "DEBUG");
-
 
         return Hazelcast.newHazelcastInstance(config);
     }
